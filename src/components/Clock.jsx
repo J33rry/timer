d693c3f675e36b9cc3f8a80664b385d21f0ac35d
Anil Kumar Meena <146952978+J33rry@users.noreply.github.com>
2025-07-27 04:48:52 +0530

    added responsize design

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -5,7 +5,9 @@ function Clock({ unit }) {
     return (
         <div className="flex flex-col items-center justify-center gap-5">
             <Flipper unit={unit} />
-            <div className="uppercase text-primary tracking-[6px]">{unit}</div>
+            <div className="uppercase text-primary tracking-[3px] sm:text-lg sm:tracking-[4px] md:tracking-[6px] text-[10px]">
+                {unit}
+            </div>
         </div>
     );
 }
--- src/components/Flipper.jsx
+++ src/components/Flipper.jsx
@@ -39,41 +39,41 @@ function Flipper({ unit }) {
 
     const currentDisplay = current.toString().padStart(2, "0");
     return (
-        <div className="relative size-44 perspective-near perspective-origin-[50%_50%] rounded-lg text-third bg-mostly_black shadow-[0_10px_4px_0_rgba(0,0,0,0.4)]">
+        <div className="relative size-20 sm:size-24 md:size-38 lg:size-44 perspective-near perspective-origin-[50%_50%] rounded-lg text-third bg-mostly_black shadow-[0_10px_4px_0_rgba(0,0,0,0.4)] text-4xl md:text-6xl lg:text-8xl">
             {/* static */}
             {/* upper */}
-            <div className="flex relative justify-between w-full h-[50%] overflow-hidden bg-Desaturated_blue items-end rounded-t-lg text-8xl -z-10">
-                <div className="bg-mostly_black h-6 w-3 rounded-r-full translate-y-1/2"></div>
+            <div className="flex relative justify-between w-full h-[50%] overflow-hidden bg-Desaturated_blue items-end rounded-t-lg -z-10">
+                <div className="bg-mostly_black h-4 w-2 md:h-6 md:w-3 rounded-r-full translate-y-1/2"></div>
                 <span className="translate-y-1/2">{currentDisplay}</span>
-                <div className="bg-mostly_black h-6 w-3 rounded-l-full translate-y-1/2"></div>
+                <div className="bg-mostly_black h-4 w-2 md:h-6 md:w-3 rounded-l-full translate-y-1/2"></div>
             </div>
             {/* lower */}
-            <div className="flex relative justify-between w-full h-[50%] overflow-hidden bg-light_blue items-start rounded-b-lg text-8xl -z-10 text-secondary ">
-                <div className="bg-mostly_black h-6 w-3 rounded-r-full -translate-y-1/2"></div>
+            <div className="flex relative justify-between w-full h-[50%] overflow-hidden bg-light_blue items-start rounded-b-lg -z-10 text-secondary ">
+                <div className="bg-mostly_black h-4 w-2 md:h-6 md:w-3 rounded-r-full -translate-y-1/2"></div>
                 <span className="-translate-y-1/2">{currentDisplay}</span>
-                <div className="bg-mostly_black h-6 w-3 rounded-l-full -translate-y-1/2"></div>
+                <div className="bg-mostly_black h-4 w-2 md:h-6 md:w-3 rounded-l-full -translate-y-1/2"></div>
             </div>
             {/* divider */}
             <div className="absolute top-1/2 h-[1px] bg-mostly_black left-0 right-0 z-10"></div>
             {/* <div className="absolute top-1/2 h-6 w-3 rounded-r-full left-0 z-10 bg-mostly_black -translate-y-1/2"></div> */}
             {/* animated*/}
             <div
-                className={`absolute flex left-0 w-full h-1/2 top-0 justify-between overflow-hidden backface-hidden text-8xl items-end origin-[50%_100%] rotate-x-0 bg-Desaturated_blue rounded-t-lg transform-3d ${
+                className={`absolute flex left-0 w-full h-1/2 top-0 justify-between overflow-hidden backface-hidden items-end origin-[50%_100%] rotate-x-0 bg-Desaturated_blue rounded-t-lg transform-3d ${
                     shuffle ? "animate-fold" : ""
                 }`}
             >
-                <div className="bg-mostly_black h-6 w-3 rounded-r-full translate-y-1/2"></div>
+                <div className="bg-mostly_black h-4 w-2 md:h-6 md:w-3 rounded-r-full translate-y-1/2"></div>
                 <span className="translate-y-1/2">{currentDisplay}</span>
-                <div className="bg-mostly_black h-6 w-3 rounded-l-full translate-y-1/2"></div>
+                <div className="bg-mostly_black h-4 w-2 md:h-6 md:w-3 rounded-l-full translate-y-1/2"></div>
             </div>
             <div
-                className={`absolute flex left-0 w-full h-1/2 top-1/2 justify-between overflow-hidden backface-hidden text-8xl items-start origin-[50%_0%] rotate-x-180 bg-light_blue rounded-b-lg transform-3d text-secondary ${
+                className={`absolute flex left-0 w-full h-1/2 top-1/2 justify-between overflow-hidden backface-hidden items-start origin-[50%_0%] rotate-x-180 bg-light_blue rounded-b-lg transform-3d text-secondary ${
                     shuffle ? "animate-unfold" : ""
                 }`}
             >
-                <div className="bg-mostly_black h-6 w-3 rounded-r-full -translate-y-1/2"></div>
+                <div className="bg-mostly_black h-4 w-2 md:h-6 md:w-3 rounded-r-full -translate-y-1/2"></div>
                 <span className="-translate-y-1/2">{currentDisplay}</span>
-                <div className="bg-mostly_black h-6 w-3 rounded-l-full -translate-y-1/2"></div>
+                <div className="bg-mostly_black h-4 w-2 md:h-6 md:w-3 rounded-l-full -translate-y-1/2"></div>
             </div>
         </div>
     );
